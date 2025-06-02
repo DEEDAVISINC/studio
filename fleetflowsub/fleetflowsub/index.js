4ebb7d77434338fd05603ee1e8a9bbcec0476e87
Dee Davis <DEEVADEEDAVIS@gmail.com>
2025-06-02 21:21:08 +0000

    Pages directory cannot be found at /home/user/studio/fleetflowsub/fleetflowsub/pages or /home/user/studio/fleetflowsub/fleetflowsub/src/pages. If using a custom path, please configure with the `no-html-link-for-pages` rule in your eslint config file.
    /home/user/studio/fleetflowsub/fleetflowsub/index.js
      10:8   error  'onRequest' is assigned a value but never used  @typescript-eslint/no-unused-vars
      10:21  error  A `require()` style import is forbidden         @typescript-eslint/no-require-imports
      11:7   error  'logger' is assigned a value but never used     @typescript-eslint/no-unused-vars
      11:16  error  A `require()` style import is forbidden         @typescript-eslint/no-require-imports
    
    ✖ 4 problems (4 errors, 0 warnings)

--- fleetflowsub/fleetflowsub/index.js
+++ fleetflowsub/fleetflowsub/index.js
@@ -1,7 +1,7 @@
 /**
  * Import function triggers from their respective submodules:
  *
- * const {onCall} = require("firebase-functions/v2/https");
+ * const {onCall} = require("firebase-functions/v2/onCall");
  * const {onDocumentWritten} = require("firebase-functions/v2/firestore");
  *
  * See a full list of supported triggers at https://firebase.google.com/docs/functions
@@ -13,7 +13,7 @@ const logger = require("firebase-functions/logger");
 // Create and deploy your first functions
 // https://firebase.google.com/docs/functions/get-started
 
-// exports.helloWorld = onRequest((request, response) => {
-//   logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+exports.helloWorld = onRequest((request, response) => {
+  logger.info("Hello logs!", {structuredData: true});
+  response.send("Hello from Firebase!");
+});
