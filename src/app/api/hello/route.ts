475dfc2fbdc7077dc8ae3fd2d4734c63d2e0f985
Dee Davis <DEEVADEEDAVIS@gmail.com>
2025-06-06 00:53:09 +0000

    import type { NextApiRequest, NextApiResponse } from 'next';
    export def

---
+++ src/app/api/hello/route.ts
@@ -0,0 +1,7 @@
+
+import { NextResponse } from 'next/server';
+import type { NextRequest } from 'next/server';
+
+export async function GET(request: NextRequest) {
+  return NextResponse.json({ message: 'Hello World!' });
+}
